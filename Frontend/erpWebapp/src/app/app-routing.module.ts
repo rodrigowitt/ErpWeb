49c5e11ec23bb2aacac3fb23fca4e34648a4ccae
Rodrigo Witthoeft <rodrigowitthoeft95@gmail.com>
2024-02-20 21:09:23 -0300

    Adição: Primeira rota e inicio de tela de pedidos

--- Frontend/erpWebapp/src/app/app-routing.module.ts
+++ Frontend/erpWebapp/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
 
-const routes: Routes = [];
+const routes:Routes = [
+  {path: 'pedidos', component: PedidosComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
--- Frontend/erpWebapp/src/app/app.module.ts
+++ Frontend/erpWebapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PedidosComponent
   ],
   imports: [
     BrowserModule,
