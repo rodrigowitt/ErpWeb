80d65782335348d88674338748374957cdac85a7
Rodrigo Witthoeft <rodrigowitthoeft95@gmail.com>
2024-04-28 18:25:03 -0300

    adição: modal de conclusão de cadastro de produto

--- Frontend/erpWebapp/src/app/components/produtos/produtos.component.ts
+++ Frontend/erpWebapp/src/app/components/produtos/produtos.component.ts
@@ -11,7 +11,13 @@ import { Produtos } from 'src/produtos';
 })
 export class ProdutosComponent {
 
-  estoque: number = 2;
+  nome!: string;
+  codigo!: string;
+  status!: string | number;
+  estoque: number = 0;
+  preco!: number;
+  estoqueProduto!: number
+  
 
   constructor(private produtosService: ProdutosService){}
 
@@ -32,11 +38,20 @@ export class ProdutosComponent {
 
 
     if (addForm.valid) {
-      console.log("estoque formulario é: " + addForm.value.estoque)
+      this.nome = addForm.value.nome
+      this.codigo = addForm.value.codigo
+      this.status = addForm.value.status
+      console.log("o valor do status ANTES de atualizar é " + this.status)
+      if (this.status == 1 || this.status == "1" ){this.status = "Ativo"} else {this.status = "Inativo"}
+      console.log("o valor do status DEPOIS é " + this.status)
+      this.preco = addForm.value.preco
+      this.estoqueProduto = addForm.value.estoque
+
         this.produtosService.addProduto(produtoApi).subscribe(
             (response: Produtos) => {
               console.log("o estoque do produto é : " + response.estoqueProduto)
                 addForm.resetForm();
+                this.openModal();
             },
             (error: HttpErrorResponse) => {
                 alert(error.message);
@@ -45,6 +60,30 @@ export class ProdutosComponent {
     } else {
         alert('Por favor, preencha todos os campos obrigatórios.');
     }
+}
+
+openModal() {
+  const modal = document.getElementById('exampleModal');
+  const body = document.body;
+  const closeButton = document.querySelector('.btn-close');
+  if (modal && body && closeButton) {
+    modal.classList.add('show');
+    modal.style.display = 'block';
+    body.classList.add('modal-open');
+    const backdrop = document.createElement('div');
+    closeButton.addEventListener('click', this.closeModal.bind(this)); // Adiciona evento de clique ao botão fechar
+  }
+}
+
+closeModal() {
+  const modal = document.getElementById('exampleModal');
+  const body = document.body;
+  const backdrop = document.querySelector('.modal-backdrop');
+  if (modal && body) {
+    modal.classList.remove('show');
+    modal.style.display = 'none';
+    body.classList.remove('modal-open');
+  }
 }
 
 }
