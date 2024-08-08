bd1403743be5efb1ee70bb05880be219383055be
Rodrigo Witthoeft <rodrigowitthoeft95@gmail.com>
2024-08-08 20:26:49 -0300

    troca de apibase

--- Backend/ErpWeb/src/main/java/com/ErpWeb/ErpWebApplication.java
+++ Backend/ErpWeb/src/main/java/com/ErpWeb/ErpWebApplication.java
@@ -20,17 +20,24 @@ public class ErpWebApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://127.0.0.1:5501","https://erp-web-three.vercel.app"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.addAllowedOriginPattern("*");
+		corsConfiguration.setAllowedHeaders(Arrays.asList(
+				"Origin",
+				"Content-Type",
+				"Accept",
+				"Authorization",
+				"X-Requested-With",
+				"Access-Control-Request-Method",
+				"Access-Control-Request-Headers"
+		));
+		corsConfiguration.setExposedHeaders(Arrays.asList(
+				"Authorization",
+				"Access-Control-Allow-Origin",
+				"Access-Control-Allow-Credentials"
+		));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(source);
 	}
-
 }
